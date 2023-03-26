83fb53fe5c4e5d97f3f891b8d494d994073e7be7
ecruzq1 <59055637+ecruzq1@users.noreply.github.com>
2023-03-26 18:41:15 -0500

    Update keys.js

--- src/keys.js
+++ src/keys.js
@@ -1,6 +1,6 @@
 module.exports={
     database:{
-        host:'hugo170983@alwaysdata.net',
+        host:'mysql-hugo170983.alwaysdata.net',
         user:'278414_inmobilia',
         password:'Pululahua.170983',
         database:'hugo170983_oportuno_inmobiliaria'
